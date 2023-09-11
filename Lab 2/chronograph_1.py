afdec34c3cd6c377acfe968585e9b693d9d58bf7
HaukeCornell <hgs52@cornell.edu>
2023-09-11 12:42:00 -0400

    Implemented screen clock and stopwatch

--- Lab 2/chronograph_1.py
+++ Lab 2/chronograph_1.py
@@ -60,6 +60,15 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonB = digitalio.DigitalInOut(board.D24)
+buttonA.switch_to_input()
+buttonB.switch_to_input()
+
+is_time_keeping = False
+start_time = 0
+pause_time = 0
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
@@ -68,6 +77,42 @@ while True:
     CURR_TIME = time.strftime("%m/%d/%Y %H:%M:%S")
     draw.text((x, y), CURR_TIME, font=font, fill="#FFFFFF")
 
+    if buttonA.value and not buttonB.value and start_time == 0:
+      print("BUTTONA")
+      start_time = time.time()
+      is_time_keeping = True
+    elif buttonA.value and not buttonB.value and start_time != 0:
+      if is_time_keeping:
+        print("BUTTONAPAUSE")
+        is_time_keeping = False
+        pause_time = time.time()
+      else:
+        print("BTTNARESUME")
+        is_time_keeping = True
+        pause_time_dur = time.time() - pause_time
+        start_time = start_time + pause_time_dur
+        pause_time = 0
+        pause_time_dur = 0
+    if buttonB.value and not buttonA.value and not is_time_keeping:
+      start_time = 0
+      pause_time = 0
+      is_time_keeping = False
+    print(is_time_keeping)
+    time_to_display = 0
+    if is_time_keeping and start_time != 0:
+      print("starttimekeep")
+      time_to_display = time.time() - start_time
+    elif not is_time_keeping and start_time == 0:
+      print("not start")
+      time_to_display = 0
+    elif not is_time_keeping and start_time != 0:
+      print("pause")
+      time_to_display = pause_time - start_time
+
+    y += 30
+    draw.text((x, y), str(time_to_display), font=font, fill="#FFFF00") 
+
     # Display image.
     disp.image(image, rotation)
-    time.sleep(1)
+    time.sleep(0.1)
+
